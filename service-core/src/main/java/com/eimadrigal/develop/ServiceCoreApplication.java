70e69eaa079b4712cc20261a1d3f16c24b33abe6
EI Madrigal <andrew_ren@163.com>
2026-01-13 20:23:12 +0800

    service-core skeleton

---
+++ service-core/src/main/java/com/eimadrigal/develop/ServiceCoreApplication.java
@@ -0,0 +1,12 @@
+package com.eimadrigal.develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCoreApplication.class, args);
+    }
+}
---
+++ service-core/src/main/java/com/eimadrigal/develop/entity/Borrower.java
@@ -0,0 +1,4 @@
+package com.eimadrigal.develop.entity;
+
+public class Borrower {
+}
