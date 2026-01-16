8845a153a650cc7212a31d68793716b9c9957152
EI Madrigal <andrew_ren@163.com>
2026-01-16 22:30:44 +0800

    service-core point level logic delete

--- service-core/src/main/java/com/eimadrigal/develop/controller/admin/AdminPointLevelController.java
+++ service-core/src/main/java/com/eimadrigal/develop/controller/admin/AdminPointLevelController.java
@@ -3,7 +3,9 @@ package com.eimadrigal.develop.controller.admin;
 import com.eimadrigal.develop.entity.PointLevel;
 import com.eimadrigal.develop.service.PointLevelService;
 import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,10 @@ public class AdminPointLevelController {
     @GetMapping("/getAllPointLevels")
     public List<PointLevel> getAllPointLevels() {
         return pointLevelService.list();
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public boolean removeById(@PathVariable Long id) {
+        return pointLevelService.removeById(id);
     }
 }
